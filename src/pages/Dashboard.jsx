56f8792d3598666d22eb87a182e6f558889a3188
mohd-ashif <mohdaszif077@gmail.com>
2024-05-10 23:31:05 +0530

    set-up & redux initialize

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+         <h1 className="text-5xl  font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/User.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>Uers</div>
+  )
+}
+
+export default Users
